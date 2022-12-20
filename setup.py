4f7a1582924bc5fbd0fd0fdb41dc286a54d3c5d3
FusionSid <siddheshadsv@icloud.com>
2022-12-20 17:43:05 +1300

    style: Format with black

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
- 
-VERSION = '0.1.1'
+
+VERSION = "0.2.1"
 DESCRIPTION = "Base 69 encoder and decoder"
 
 setup(
-    name='Base_69',
+    name="Base_69",
     version=VERSION,
-    author='Michael Parker',
-    author_email='michaelrbparker@protonmail.com',
-    url='https://github.com/micfun123/Base69',
+    author="Michael Parker",
+    author_email="michaelrbparker@protonmail.com",
+    url="https://github.com/micfun123/Base69",
     description=DESCRIPTION,
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    packages=find_packages()
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
 )
 
 # python3 setup.py bdist_wheel
