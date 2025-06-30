f76cf59ce21655410ea79b86e7e87e7f51a5424b
Michael <michaelrbparker@protonmail.com>
2025-06-30 11:20:40 +0100

    69

--- base69/base69.py
+++ base69/base69.py
@@ -2,20 +2,19 @@ import string
 
 __all__ = ["encode_base69", "decode_base69", "InvalidInput"]
 
+# Base-69 character set (69 unique characters)
 values = (
     list(string.digits)
     + list(string.ascii_uppercase)
     + list(string.ascii_lowercase)
-    + ["+", "/", "=", "@", "*", "-", "!", "#"]
+    + ["+", "/", "=", "@", "*", "-", "!", "]"]
 )
 CONVERSION_TABLE = dict(zip(range(len(values)), values))
-
 
 class InvalidInput(Exception):
     def __init__(self, message: str) -> None:
         message = f"Invalid input was provided! ({message})"
         super().__init__(message)
-
 
 def encode_base69(input: int) -> str:
     """
@@ -30,18 +29,22 @@ def encode_base69(input: int) -> str:
     Raises:
         `InvalidInput`: If the input is invalid
     """
-
-    if not type(input) == int:
+    if not isinstance(input, int):
         raise InvalidInput("Must be of type: int")
+
+    if input < 0:
+        raise InvalidInput("Input must be non-negative")
+
+    if input == 0:
+        return "69*|" + CONVERSION_TABLE[0]
 
     base69 = ""
     while input > 0:
-        remainder = input % 70
+        remainder = input % 69
         base69 = CONVERSION_TABLE[remainder] + base69
-        input = input // 70
-    base69 = "69*|" + base69
-    return base69
+        input = input // 69
 
+    return "69*|" + base69
 
 def decode_base69(input: str) -> int:
     """
@@ -56,19 +59,20 @@ def decode_base69(input: str) -> int:
     Raises:
         `InvalidInput`: If the input is invalid
     """
+    if not isinstance(input, str):
+        raise InvalidInput("Must be of type: str")
 
     if not input.startswith("69*|"):
-        raise InvalidInput("Invalid Base69 input! Base69 must begin '69*|'")
+        raise InvalidInput("Invalid Base69 input! Base69 must begin with '69*|'")
 
     input = input[4:]
-
     base = 0
-    for i in range(len(input)):
+
+    for i, char in enumerate(reversed(input)):
         try:
-            base += list(CONVERSION_TABLE.keys())[
-                list(CONVERSION_TABLE.values()).index(input[i])
-            ] * (70 ** (len(input) - i - 1))
-        except ValueError as ve:
-            invalid_char = ve.args[0].split(" ")[0]
-            raise InvalidInput(f"{invalid_char} is not allowed")
+            value = list(CONVERSION_TABLE.keys())[list(CONVERSION_TABLE.values()).index(char)]
+            base += value * (69 ** i)
+        except ValueError:
+            raise InvalidInput(f"'{char}' is not a valid Base69 character")
+
     return base
